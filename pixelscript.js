e767eed2963f33ccede68d35394994fa312b0c1e
Jayme Ramsay <jayme.ramsay@gmail.com>
2017-08-25 22:07:57 -0500

    burned down my last pushand refactored all of my js/css/html. Pixel art is functional with clicks and itis able to color pixels to the border. yay

--- pixelscript.js
+++ pixelscript.js
@@ -1,64 +1,71 @@
-window.onload = function () {
-  //Creates click event for colors onto pixels
-  let body = document.body;
-  let currentColor = null;
-  //create an array with the colors in it to generate the pen divs
-  function createPalette() {
-    let palettes = ["hotpink", "red", "darkorange", "gold", "yellow", "yellowgreen", "green",
-      "royalblue", "blue", "darkblue", "indigo", "purple", "peru", "brown",
-      "saddlebrown", "black", "gray", "white"
-    ];
+let body = document.body;
+let currentColor = '#030303';
 
-    //loop through the color palette
-    palettes.forEach(palette => {
-      let myDiv = document.createElement('div');
-      myDiv.setAttribute('class', palette);
-      console.log(myDiv);
-      // create click event
-      myDiv.addEventListener("click", function (event) {
-        let element = event.target;
-        console.log(this.style.backgroundColor);
-        currentColor = element.classList[0];
-        console.log('I CLICK IT!');
-        // console.log("current THIS!", this);
-      })
-      body.append(myDiv);
-    })
-  }
-  createPalette();
+//set and style a canvas
+//create a canvas ID
+//create function pixels/squares divs
+let canvas = document.getElementById('canvas');
+//create for loop to loop through squares and attach event listeners
+for (let i = 0; i < 3150; i++) {
+  let square = document.createElement('div');
+  square.className = 'square';
+  square.addEventListener('click', function (ev) {
+    ev.target.style.backgroundColor = currentColor;
+    ev.target.style.borderColor = currentColor;
+  })
+  canvas.appendChild(square);
+}
 
-  let container = document.createElement('div');
-  container.setAttribute('class', "container");
-  body.append(container);
+//set and style a palette
 
-  for (let x = 0; x < 19; x++) {
-    let row = document.createElement('div');
-    row.setAttribute('class', "row");
-    container.append(row);
-    for (let y = 0; y < 92; y++) {
-      let square = document.createElement('div');
-      square.setAttribute('class', 'square');
-      row.append(square);
-      square.addEventListener('click', function (event) {
-        let element = event.target;
-        console.log(currentColor);
-        element.style.backgroundColor = currentColor;
-        element.style.borderColor = currentColor;
-        console.log("CLICKED!");
-      })
-    }
-  }
+//create color buttons in an array;
+let colors = [
+  '#ad2105',
+  '#ff0000',
+  '#ff5900',
+  '#ff8c00',
+  '#ffbb00',
+  '#ffea00',
+  '#a5f53c',
+  '#34cc02',
+  '#289101',
+  '#066601',
+  '#03ad99',
+  '#04ffee',
+  '#08c1ff',
+  '#0891bf',
+  '#045cff',
+  '#1f02e0',
+  '#c78fff',
+  '#8902ff',
+  '#622d9e',
+  '#a11163',
+  '#ff99ee',
+  '#CD853F',
+  '#A0522D',
+  '#8B4513',
+  '#D2B48C',
+  '#fafffa',
+  '#90918f',
+  '#61615f',
+  '#424241',
+  '#030303',
+];
 
-  // }
 
-  // let penColor = "black";
-  // function setPenColor(pen) {
-  //   penColor = pen;
-  // }
-  //
-  // function setPixelColor(pixel) {
-  //   pixel.style.backgroundColor = penColor;
-  // }
-  //create all classes to match color
-
+//create for loop to iterate over buttons
+//creating loops because I'll only need to use them once therefore a function isn't really necessary
+let palette = document.getElementById('palette');
+for (let i = 0; i < colors.length; i++) {
+  //attach colors
+  // let pen = colors[i];
+  let pen = document.createElement('div');
+  pen.className = 'pen';
+  pen.style.backgroundColor = colors[i];
+  pen.addEventListener('click', function (ev) {
+    currentColor = ev.target.style.backgroundColor;
+  })
+  palette.appendChild(pen);
+  console.log(pen + '<<<<WE DID IT');
+  //attach event listeners
 }
